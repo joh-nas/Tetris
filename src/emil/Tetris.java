088bdaab0412d323d95f63467deae9879efd0ce3
joh-nas <jarnnia01>
2018-03-11 21:36:10 +0100

    Fixed formatting

--- src/emil/Tetris.java
+++ src/emil/Tetris.java
@@ -2,7 +2,9 @@ package emil;
 import javax.swing.JFrame;
 
 public class Tetris extends JFrame {
-		final static protected int aik = 2;
+	
+	final static protected int aik = 2;
+	
 	public Tetris () {
 		Board board = new Board();
 		add(board);
